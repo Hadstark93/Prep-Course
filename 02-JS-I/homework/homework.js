4dca56c68aebd13d61c1929be54bea8d32a4f6ff
Arturo <amunoz.lagos93@gmail.com>
2022-09-24 14:47:37 -0300

    Test Listos Homework I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -181,13 +181,14 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
 
-  if (numero<=1){return "Es positivo";}
-  else if (numero>1){return "Es negativo";}
-  else{return false;}
+  if (numero>0){
+    return "Es positivo";
+  }else if (numero<0){
+    return "Es negativo";
+  }else{return false;}
 
   
 }
-esPositivo(9);
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
@@ -216,6 +217,8 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+
+  return alto * ancho;
   
 }
 
@@ -224,12 +227,17 @@ function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
   
+  return lado*4;
+
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+
+  return (base*altura)/2
+
 
 }
 
@@ -238,6 +246,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+
+
+  return euro*1.20;
   
 }
 
@@ -249,6 +260,15 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   
+
+    if (letra == "a" || letra =="e" || letra =="i" || letra=="o" || letra =="u"
+          ||letra == "A" || letra =="E" || letra =="I" || letra=="O" || letra =="U"){
+          return "Es vocal";
+    }else if(letra.lenght!=1){
+      return "Dato incorrecto";
+    }else{
+      return "Dato incorrecto";
+    } 
 }
 
 
